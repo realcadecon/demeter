19f390bbc7faf46650d7e3a1a41ef7ee838b6b49
concad <cadeconner8@gmail.com>
2024-02-27 15:17:43 -0600

    - clean up some unused imports and warnings

--- src/main/java/com/callisto/demeter/security/SecurityConfig.java
+++ src/main/java/com/callisto/demeter/security/SecurityConfig.java
@@ -6,13 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
