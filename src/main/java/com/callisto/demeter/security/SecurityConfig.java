683b57af9a5dd03c826f37922a096519e065172f
concad <cadeconner8@gmail.com>
2024-02-27 15:11:55 -0600

    - updated login page with html/css - updated spring security to always create a session   - fixes error when logging out and not being able to load thymeleaf forms

--- src/main/java/com/callisto/demeter/security/SecurityConfig.java
+++ src/main/java/com/callisto/demeter/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -25,6 +26,8 @@ public class SecurityConfig {
                                 .requestMatchers("/processRegistration").permitAll()
                                 .requestMatchers("/css/**").permitAll()
                                 .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 )
                 .formLogin(form ->
                         form
